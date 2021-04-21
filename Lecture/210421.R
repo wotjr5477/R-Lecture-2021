c67eadc4e2713bdf88ca35ee9d1e2b1e2ece1c5a
Junseok Ban <wotjr5477@naver.com>
2021-04-21 11:36:48 +0900

    chapter 2

---
+++ Lecture/210421.R
@@ -0,0 +1,40 @@
+#디렉토리
+getwd()
+# a = choose.dir()
+# setwd(a)
+#getwd()
+
+# 패키지
+search()
+# 패키지 설치
+# install.packages()
+## 패키지 적용
+library(dplyr)
+library(ggplot2)
+search()
+
+iris
+str(iris)
+
+head(iris) #default = 6
+head(iris,10)
+tail(iris)
+plot(iris)
+
+# 두 속성의 상관 관계
+plot(iris$Petal.Length, iris$Petal.Width, col=iris$Species, pch = 18 )
+
+
+# tips.csv download
+tips = read.csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv')
+str(tips)
+
+summary(tips)
+
+# ggplot2로
+tips %>% ggplot(aes(size))+geom_histogram() #히스토그램
+tips %>% ggplot(aes(total_bill, tip))+geom_point() #산점도
+tips %>% ggplot(aes(total_bill, tip))+geom_point(aes(col=day)) #포인트 색지정
+tips %>% ggplot(aes(total_bill, tip))+geom_point(aes(col=day, pch=sex)) #포인트 성별로 분류
+tips %>% ggplot(aes(total_bill, tip))+geom_point(aes(col=day, pch=sex),size=3) #포인트 사이즈 설정
+
---
+++ intro(210420).R
@@ -0,0 +1,24 @@
+# 김철기
+# ckiekim@naver.com
+# 010
+# 노트북 번호 : 1503003-183604952
+
+install.packages("dplyr")
+install.packages("ggplot2")
+cars
+str(cars)
+
+plot(cars,col = 'blue', xlab = "속도", ylab = "거리")
+
+women
+print(women)
+
+#구조
+str(cars)
+
+#디렉터리 지정
+getwd()
+cdwd()
+a=choose.dir()
+setwd(a)
+getwd()
