6f7f2e46a541714c2aaa1cd3d9845731e1ab6cba
Junseok Ban <wotjr5477@naver.com>
2021-05-07 17:18:52 +0900

    alice

--- Lecture/Alice wordcloud.R
+++ Lecture/Alice wordcloud.R
@@ -1,35 +1,53 @@
 # Alice
 library(dplyr)
-install.packages("readr")
+#install.packages("readr")
 library(readr)
 library(devtools)
-devtools::install_github("lchiffon/wordcloud2")
+#devtools::install_github("lchiffon/wordcloud2")
+library(wordcloud2)
+library(tm) # text mining library
+library(SnowballC)
+
 # wordcloud2(alice, figPath = "data/Alice_mask.png")
 
 
-alice = read_file("data/Alice.txt")
+alice = read_file("data/Alice.txt") # 파일 채로 읽음
+head(alice)
 
-library(tm)
-library(SnowballC)
+ali = readLines("data/Alice.txt") # 줄별로 읽어들임
+head(ali)
 
 al = Corpus(VectorSource(alice))
-al
 
-al = tm_map(al,stripWhitespace) # 공백제거
 al = tm_map(al, tolower) # 소문자화
+#al = tm_map(al, content_transformer(tolower)) # 소문자화
 al = tm_map(al, removeNumbers) # 숫자제거거
-al = tm_map(al, removeWords, stopwords("en")) # 영문 불용어
+al = tm_map(al, removeWords, c(stopwords("en"), "said"))# 영문 불용어
 al = tm_map(al, removePunctuation) # 구두점 제거
+al = tm_map(al, stripWhitespace) # 공백제거
 
-al = tm_map(al, stemDocument) # 어근 추출 / SnowballC 필요
+#al = tm_map(al, stemDocument) # 어근 추출 / SnowballC 필요
 
-dtm = TermDocumentMatrix(al) # DTM 만들기
+dtm = DocumentTermMatrix(al) # DTM 만들기
 dtm
-dtm = as.matrix(dtm)
-head(dtm)
-
-rowSums(dtm)
+dtm = as.matrix(dtm) # 행열로
+dim(dtm)
 
 v = sort(rowSums(dtm), decreasing = T)
 d = data.frame(word = names(v), freq = v)
 head(d)
+
+wordcloud2(d)
+
+
+tdm = TermDocumentMatrix(al)
+tdm
+tdm = as.matrix(tdm)
+dim(tdm)
+
+v2 = sort(rowSums(tdm), decreasing = T)
+d2 = data.frame(word = names(v2), freq = v2)
+head(d2)
+
+wordcloud2(d2)
+
